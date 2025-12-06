8bf75e09e65c4e4b1e3dcba8b5875adcfa6e6824
Karim-308 <2001118@eng.asu.edu.eg>
2025-12-06 20:04:12 +0200

    Remove MultiList class and related files; update main.cpp to utilize Value class instead

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -1,43 +1,17 @@
-#include <any>
 #include <iostream>
 #include <string>
+#include "Value.h"
 
-#include "MultiList.h"
+using namespace std;
 int main() {
-    MultiList list;
+    Value val1("Karim iBRAHIM");
+    Value* val2 = new Value("opnes source intake 46");
+    Value val3;
 
-    std::cout << "Pushing 42 \"hello\" 3.14 true\n";
-    list.push(42);
-    list.push(std::string("Mina"));
-    list.push(3.14);
-    list.push(true);
-    list.print();
+    cout << "Value data: " << val1 << endl;
+    cout << "Value data: " << *val2 << endl;
+    cout << "Value data: " << val3 << endl;
 
-    std::cout << "Inserting 'Karim' at index 2...\n";
-    list.insert(2, std::string("Karim"));
-    list.print();
-
-    std::cout << "get(0): " << std::any_cast<int>(list.get(0)) << '\n';
-    std::cout << "operator[](2): " << std::any_cast<std::string>(list[2]) << '\n';
-    list[0] = 100;
-    std::cout << "After modifying index 0 via operator[]: "
-              << std::any_cast<int>(list.get(0)) << '\n';
-
-    std::cout << "Current size: " << list.size() << '\n';
-
-    std::cout << "Popping last element...\n";
-    bool popped = std::any_cast<bool>(list.pop());
-    std::cout << "Popped: " << (popped ? "true" : "false") << '\n';
-    list.print();
-
-    std::cout << "Removing element at index 1...\n";
-    list.remove(1);
-    list.print();
-
-    std::cout << "Clearing list...\n";
-    list.clear();
-    std::cout << "Size after clear: " << list.size() << '\n';
-    list.print();
-
+    delete val2;
     return 0;
 }
